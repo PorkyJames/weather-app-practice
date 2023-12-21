81d7c9496549a5525324b5abe5cad12bc7f54944
PorkyJames <kwanjko17@gmail.com>
2023-12-20 19:13:10 -0800

    Logic for Temp change is working. The Calculation is incorrect

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
+const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 
 //! Create temp in F and Temp in C to toggle back and forth
 let tempInF
 let tempInC
+let isFahrenheit = true;
+
+//! Let's split up our original addEventListener into two separate functions. 
 
 //! Create a function that will allow us to update the display of the weather
 const updateWeatherDisplay = (json) => {
@@ -24,83 +28,142 @@ const updateWeatherDisplay = (json) => {
     tempInF = parseInt(json.main.temp)
     tempInC = parseInt((tempInF - 32) * (5 / 9))
 
-    temperature.innerHTML = `${isFahrenheit ? temperatureInFahrenheit : temperatureInCelsius}<span>°${isFahrenheit ? 'F' : 'C'}</span>`;
+    temperature.innerHTML = `${isFahrenheit ? tempInF : tempInC}<span>°${isFahrenheit ? 'F' : 'C'}</span>`;
+    
+    switch (json.weather[0].main) {
+        case 'Clear':
+            image.src = 'images/clear.png';
+            break;
+
+        case 'Rain':
+            image.src = 'images/rain.png';
+            break;
+
+        case 'Snow':
+            image.src = 'images/snow.png';
+            break;
+
+        case 'Clouds':
+            image.src = 'images/cloud.png';
+            break;
+
+        case 'Haze':
+            image.src = 'images/mist.png';
+            break;
+
+        default:
+            image.src = '';
+    }
+
+    description.innerHTML = `${json.weather[0].description}`;
+    humidity.innerHTML = `${json.main.humidity}%`;
+    wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+    weatherBox.style.display = '';
+    weatherDetails.style.display = '';
+    weatherBox.classList.add('fadeIn');
+    weatherDetails.classList.add('fadeIn');
+    container.style.height = '590px';
 }
 
-//! Event listener for when we click on the search. Really only need one since we're working with only using one button to look for our weather. 
-search.addEventListener('click', () => {
-
-    //! Establish that our city is whatever the value is in our search box input's value. 
-    const city = document.querySelector('.search-box input').value;
-
-    //! If there isn't anything in our input, then we'll just return. So this deals with the edge case of there being nothing. 
-    if (city === '')
-        return;
-
-    //! If we do include anything, then we're going to first fetch our api website. 
-    //! After we do that, we're going to use .then() and get the response of that and then .json() it so we can parse it. 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`)
+// Function to fetch weather data
+function fetchWeather(city) {
+    const units = isFahrenheit ? 'imperial' : 'metric';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${APIKey}`)
         .then(response => response.json())
         .then(json => {
-            
-            //! json.cod is very specific to the weather app. From what I've read online, cod is based on 
-            //! the HTTP status code of the API response. It's not related to the weather data itself, but provides info
-            //! about the success or failure of the API Call. 
-            if (json.cod === '404') {
-                container.style.height = '400px';
-                weatherBox.style.display = 'none';
-                weatherDetails.style.display = 'none';
-                error404.style.display = 'block';
-                error404.classList.add('fadeIn');
-                return;
-            }
-
-            error404.style.display = 'none';
-            error404.classList.remove('fadeIn');
-
-            const image = document.querySelector('.weather-box img');
-            const temperature = document.querySelector('.weather-box .temperature');
-            const description = document.querySelector('.weather-box .description');
-            const humidity = document.querySelector('.weather-details .humidity span');
-            const wind = document.querySelector('.weather-details .wind span');
-
-
-        //! Switch cases are for establishing if each case is met. For example, if the weather is sunny, we'll set our image as the sunny icon.
-            switch (json.weather[0].main) {
-                case 'Clear':
-                    image.src = 'images/clear.png';
-                    break;
-
-                case 'Rain':
-                    image.src = 'images/rain.png';
-                    break;
-
-                case 'Snow':
-                    image.src = 'images/snow.png';
-                    break;
-
-                case 'Clouds':
-                    image.src = 'images/cloud.png';
-                    break;
-
-                case 'Haze':
-                    image.src = 'images/mist.png';
-                    break;
-
-                default:
-                    image.src = '';
-            }
-
-            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°F</span>`;
-            description.innerHTML = `${json.weather[0].description}`;
-            humidity.innerHTML = `${json.main.humidity}%`;
-            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
-
-            weatherBox.style.display = '';
-            weatherDetails.style.display = '';
-            weatherBox.classList.add('fadeIn');
-            weatherDetails.classList.add('fadeIn');
-            container.style.height = '590px';
+            updateWeatherDisplay(json);
         });
+}
 
+// Search button event listener
+search.addEventListener('click', () => {
+    const city = document.querySelector('.search-box input').value;
+    if (city !== '') fetchWeather(city);
 });
+
+// Temperature toggle button event listener
+toggleSwitch.addEventListener('change', () => {
+    isFahrenheit = !isFahrenheit;
+    const city = document.querySelector('.search-box input').value;
+    if (city) fetchWeather(city);
+});
+
+
+// //! Event listener for when we click on the search. Really only need one since we're working with only using one button to look for our weather. 
+// search.addEventListener('click', () => {
+
+//     //! Establish that our city is whatever the value is in our search box input's value. 
+//     const city = document.querySelector('.search-box input').value;
+
+//     //! If there isn't anything in our input, then we'll just return. So this deals with the edge case of there being nothing. 
+//     if (city === '')
+//         return;
+
+//     //! If we do include anything, then we're going to first fetch our api website. 
+//     //! After we do that, we're going to use .then() and get the response of that and then .json() it so we can parse it. 
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`)
+//         .then(response => response.json())
+//         .then(json => {
+            
+//             //! json.cod is very specific to the weather app. From what I've read online, cod is based on 
+//             //! the HTTP status code of the API response. It's not related to the weather data itself, but provides info
+//             //! about the success or failure of the API Call. 
+//             if (json.cod === '404') {
+//                 container.style.height = '400px';
+//                 weatherBox.style.display = 'none';
+//                 weatherDetails.style.display = 'none';
+//                 error404.style.display = 'block';
+//                 error404.classList.add('fadeIn');
+//                 return;
+//             }
+
+//             error404.style.display = 'none';
+//             error404.classList.remove('fadeIn');
+
+//             const image = document.querySelector('.weather-box img');
+//             const temperature = document.querySelector('.weather-box .temperature');
+//             const description = document.querySelector('.weather-box .description');
+//             const humidity = document.querySelector('.weather-details .humidity span');
+//             const wind = document.querySelector('.weather-details .wind span');
+
+
+//         //! Switch cases are for establishing if each case is met. For example, if the weather is sunny, we'll set our image as the sunny icon.
+//             switch (json.weather[0].main) {
+//                 case 'Clear':
+//                     image.src = 'images/clear.png';
+//                     break;
+
+//                 case 'Rain':
+//                     image.src = 'images/rain.png';
+//                     break;
+
+//                 case 'Snow':
+//                     image.src = 'images/snow.png';
+//                     break;
+
+//                 case 'Clouds':
+//                     image.src = 'images/cloud.png';
+//                     break;
+
+//                 case 'Haze':
+//                     image.src = 'images/mist.png';
+//                     break;
+
+//                 default:
+//                     image.src = '';
+//             }
+
+//             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°F</span>`;
+//             description.innerHTML = `${json.weather[0].description}`;
+//             humidity.innerHTML = `${json.main.humidity}%`;
+//             wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+//             weatherBox.style.display = '';
+//             weatherDetails.style.display = '';
+//             weatherBox.classList.add('fadeIn');
+//             weatherDetails.classList.add('fadeIn');
+//             container.style.height = '590px';
+//         });
+
+// });
